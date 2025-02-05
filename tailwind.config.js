13c88d00f11c8ce81cad2680e024d04b089ff190
WangWang0226 <eeha8834@gmail.com>
2025-02-05 17:22:02 -0500

    fix[frontend]: fix bg photo not showing issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         secondary: '#3965a2'
       },
       backgroundImage: {
-        'profile00': "url('/images/profile00.jpg')",
+        'profile00': "url('/images/profile00.jpeg')",
         'profile01': "url('/images/profile01.jpeg')",
         'profile02': "url('/images/profile02.jpg')",
       }
